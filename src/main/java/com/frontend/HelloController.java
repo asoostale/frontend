18f9d5923efa66f6319954bbb5141f7673b4ecef
SooYoung Joo <asoostale@naver.com>
2024-02-22 11:06:30 +0900

    초안파일

---
+++ src/main/java/com/frontend/HelloController.java
@@ -0,0 +1,13 @@
+package com.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "2.box";
+    }
+}
